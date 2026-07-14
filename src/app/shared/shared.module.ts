5c4d245fbf5536a5544111b9659cfc51b3d3ae3a
yashgoel-jpg <yash.goel@joshtechnologygroup.com>
2026-07-14 17:06:17 +0530

    started header

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './components/button/button.component';
 import { MatButtonModule } from '@angular/material/button';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-    declarations: [ButtonComponent],
+    declarations: [ButtonComponent, HeaderComponent],
     imports: [CommonModule, MatButtonModule],
-    exports: [ButtonComponent],
+    exports: [ButtonComponent, HeaderComponent],
 })
 export class SharedModule {}
